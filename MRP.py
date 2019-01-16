c02825fd5bbe55acfd9341bf96d8425bd8d2cd82
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-15 21:29:02 -0800

    Update MRP.py

--- MRP.py
+++ MRP.py
@@ -4,13 +4,16 @@
 # This code returns an accumulated reward as well (does not use the reward from the starting point though
 # This code is basically the same as the MP, just with and added reward
 
-def MarkovReward(changes,P,transitions,reward,start,discount):
+def MarkovReward(changes,P,states,transitions,reward,start,discount):
     import numpy as np
     import random
     # Changes = amount of times the process will change (integer)
     # P = transition matrix (matrix)
+    # states = possible states (list)
     # transitions = possible transitions in matrix, e.g. frome state A to B and so forth (strings)
+    # reward = reward at each state (list of numerics values)
     # start = start value in transition matrix (string)
+    # discount = discount factor between each state (numeric)
     i = 0
     accumulated_reward = 0
     path=[start]
@@ -36,17 +39,17 @@ def MarkovReward(changes,P,transitions,reward,start,discount):
 
 #My MRP
 
-states=["School", "Game", "Food", "Party"]
+states=[["School", "Game", "Food", "Party"],[5, 3, 1, -4]]
 transitions=[["SS","SG","SF","SP"],["GS","GG","GF","GP"],["FS","FG","FF","FP"],["PS","PG","PF","PP"]]
 P=[[0.5,0.2,0.3,0],[0,0,0.5,0.5],[0.3,0.3,0.1,0.3],[0,0.2,0.5,0.3]]
-reward=[5,3,1,-4]
 
 
-q,z=MarkovReward(7,P,transitions,reward,states[3],0.99)
+q,z=MarkovReward(7,P,states[0],transitions,states[1],states[0][3],0.99)
 
 # It will return something like:
+The procces resulted in a reward of:  9.69590700352197 .
 q=['Party', 'Food', 'Party', 'Food', 'Party', 'Food', 'Party', 'Game']
-z=-5.70535023979701
+z=9.69590700352197 .
 
 
 
