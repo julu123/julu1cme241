6ac765e80fb707f112fc1144463339b8653e2389
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-15 22:55:40 -0800

    Update MRP.py

--- MRP.py
+++ MRP.py
@@ -14,24 +14,33 @@ def MarkovReward(changes,P,states,transitions,reward,start,discount):
     # reward = reward at each state (list of numerics values)
     # start = start value in transition matrix (string)
     # discount = discount factor between each state (numeric)
+    final_step = 0
     accumulated_reward = 0
     path=[start]
     current_activity = start
-    for i in range(changes):
+    i = 0
+    while i < changes:
         for j in range(len(P)):
             if current_activity == states[j]:
                 #print("j:", j )
                 RV = np.random.choice(transitions[j],replace=True,p=P[j])
                 for k in range(len(P)):
                     if RV == transitions[j][k]:
+                        if P[j][k] == 1 and k == j:
+                            i = changes
+                            print("The procces reached the termination state", "'",states[j],"'", "after", final_step, "steps.")
+                            break
                         path.append(states[k])
                         current_activity = states[k]
                         accumulated_reward += reward[k]
-                        if i > 1:
+                        if i > 0:
                             accumulated_reward = accumulated_reward*discount
                         #print("k:", k)
+                        final_step += 1
                         break
                 break
+        i += 1
+    print("The path was:", path)
     print("The procces resulted in a reward of: ", accumulated_reward, ".")
     return(path,accumulated_reward)
 
@@ -46,9 +55,9 @@ P=[[0.5,0.2,0.3,0],[0,0,0.5,0.5],[0.3,0.3,0.1,0.3],[0,0.2,0.5,0.3]]
 q,z=MarkovReward(7,P,states[0],transitions,states[1],states[0][3],0.99)
 
 # It will return something like:
-The procces resulted in a reward of:  9.69590700352197 .
-q=['Party', 'Food', 'Party', 'Food', 'Party', 'Food', 'Party', 'Game']
-z=9.69590700352197 .
+The procces reached the termination state ' Game ' after 6 steps.
+The path was: ['School', 'School', 'School', 'School', 'School', 'School', 'Game']
+The procces resulted in a reward of:  28 .
 
 
 
