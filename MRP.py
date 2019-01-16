decc82ea35176ba48885d68835b043ba7fb9817c
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-15 21:18:36 -0800

    Add files via upload

---
+++ MRP.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[278]:
+
+
+def MarkovReward(changes,P,transitions,reward,start,discount):
+    import numpy as np
+    import random as rm
+    # Changes = amount of times the process will change (integer)
+    # P = transition matrix (matrix)
+    # transitions = possible transitions in matrix, e.g. frome state A to B and so forth (strings)
+    # start = start value in transition matrix (string)
+    i = 0
+    accumulated_reward = 0
+    path=[start]
+    current_activity = start
+    while i < changes:
+        for j in range(len(P)):
+            if current_activity == states[j]:
+                #print("j:", j )
+                RV = np.random.choice(transitions[j],replace=True,p=P[j])
+                for k in range(len(P)):
+                    if RV == transitions[j][k]:
+                        path.append(states[k])
+                        current_activity = states[k]
+                        accumulated_reward += reward[k]
+                        accumulated_reward = accumulated_reward*discount
+                        #print("k:", k)
+                        break
+                break
+        i += 1
+    print("The procces resulted in a reward of: ", accumulated_reward, ".")
+    return(path,reward)
+
+
+# In[279]:
+
+
+states=["School", "Game", "Food", "Party"]
+transitions=[["SS","SG","SF","SP"],["GS","GG","GF","GP"],["FS","FG","FF","FP"],["PS","PG","PF","PP"]]
+P=[[0.5,0.2,0.3,0],[0,0,0.5,0.5],[0.3,0.3,0.1,0.3],[0,0.2,0.5,0.3]]
+reward=[5,3,1,-4]
+
+
+# In[287]:
+
+
+q,z=MarkovReward(7,P,transitions,reward,states[3],0.99)
+
+
+# In[ ]:
+
+
+
+
