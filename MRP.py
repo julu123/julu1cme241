076824a719cab870b2c031cdc4a93cdda8ec54cd
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-23 22:04:34 -0800

    MRP_A

--- MRP.py
+++ MRP.py
@@ -7,111 +7,82 @@
 import numpy as np
 from typing import TypeVar, Dict, List
 
-
-# In[777]:
-
-
-State = TypeVar('S')
-States = List[State]
-Transitions = Dict[State,Dict[State,(int or float)]]
+Transitions_rewards = Dict[Transitions,(float or int)] # For A where each state has a R(s) 
+Rewards = List[float]
 
 
-# In[816]:
-
-
-class MP(object):
-    #Defined by probability distribution (P) 
-    #states (S) are not required
-    def __init__(self,ProbDist:(Transitions or np.ndarray),S:States=None,print_text=False):
+class MRP_A(MP):
+    def __init__(self,ProbDist:(Transitions_rewards or np.ndarray),gamma:float=1,R:Rewards=None, S:States=None, print_text=False):
         if S == None:
             if isinstance(ProbDist, np.ndarray) == False:
                 self.States=list(ProbDist)
             else:
-                self.States=[i for i in range(len(P))]
+                self.States=[i for i in range(len(ProbDist))]
         else:
             self.States=S
-        if isinstance(ProbDist, np.ndarray) == True:
-            self.ProbDist = ProbDist
+        if R == None:
+            Rew=[]
+            for i in ProbDist:
+                for j in ProbDist[i]:
+                    if isinstance(j, (int,float)) == True:
+                        Rew.append(j)
+            self.Rewards = Rew
         else:
-            self.ProbDist=self.Transition_matrix_to_array(ProbDist)
+            self.Rewards = Rewards
+        self.ProbDist=self.Get_ProbDist(ProbDist)
+        self.gamma=gamma
         self.print_text=print_text
-        if self.print_text == True:
-            print('You have created a new Markov process. It has ',len(self.States), 'states.')
+        
+    def Get_Value_Function(self,n:int=None):
+        if np.linalg.det(self.ProbDist) > 1e-5:
+            print("yolo")
+            R=np.dot(self.ProbDist,self.Rewards)
+            inverse=np.linalg.inv(np.identity(len(self.States))-self.gamma*self.ProbDist)
+            return(np.dot(inverse,R))
+        else:
+            print("Determinant Zero -- wait for simulation")
+            VF=np.zeros((len(self.States),1))
+            for item in self.States:
+                if n == None:
+                    n=1000
+                test=0
+                for i in range(n):
+                    test+=self.Simulate_Rewards(n,item)
+                VF[self.States.index(item)]=test/n
+            return(VF)
     
-    def Transition_matrix_to_array(self,ProbDist):
-        assert isinstance(P,dict) == True
+    def Get_ProbDist(self,ProbDist):
+        assert isinstance(ProbDist,dict) == True
         Trans_mat=np.zeros((len(self.States),len(self.States)))
         for i in ProbDist:
-            for j in ProbDist[i]:
-                Trans_mat[self.States.index(i)][self.States.index(j)]=ProbDist[i][j]
+            for j in ProbDist[i][0]:
+                Trans_mat[self.States.index(i)][self.States.index(j)]=ProbDist[i][0][j]
         return(Trans_mat)
         
-    def Generate_Stationary_Dist(self,choice='dict') -> Dict[State,float]:
-        eigenvalues, eigenvectors = np.linalg.eig(self.ProbDist.T)
-        stat_dist=np.zeros((len(self.States),1))
-        for i in range(len(eigenvalues)):
-            if abs(eigenvalues[i]-1) < 1e-8:
-                stat_dist = stat_dist + eigenvectors[:,i].reshape(len(self.States),1)
-        output_array = (stat_dist/np.sum(stat_dist)).real
-        if choice == 'dict':
-            return {self.States[i]:round(float(j),4) for i,j in enumerate(output_array)}
-        elif choice == 'array':
-            return output_array
-    def Simulate(self,steps,start:State=None,print_text=None):
-        if start == None:
-            start = self.States[0]
-        elif isinstance(start, (int,float)) == True:
-            start = self.States[start]
-        if print_text == None:
-            print_text = self.print_text
-        path = [start]
-        current_activity = start
+    def Simulate_Rewards(self,steps:int=10,start:State=None,print_text=False):
+        if isinstance(start,(int,float)) == True:
+            start=self.States[int(start)]
+        path = self.Simulate(steps,start,print_text)
+        accumulated_reward=0
         i=0
-        while i < steps:
-            for j in range(len(self.States)):
-                if current_activity == self.States[j]:
-                    RV = np.random.choice(self.States,replace=True,p=self.ProbDist[j])
-                    for k in range(len(self.States)):
-                        if RV == self.States[k]:
-                            if self.ProbDist[j][k] == 1 and k == j:
-                                if print_text == True:
-                                    print("The procces reached the termination state", "'",self.States[j],"'", "after", i, "steps.")
-                                i = steps
-                                break
-                            path.append(self.States[k])
-                            current_activity = self.States[k]
-                            break
-                    break
-            i += 1
+        for item in path:
+            path_ind=self.States.index(item)
+            accumulated_reward=accumulated_reward+self.Rewards[path_ind]*self.gamma**i
+            i+=1
         if print_text == True:
-            print("The path was:", path)
-        return(path)
-        
+            print('The path Resulted in a value of ', float(accumulated_reward))
+        return(float(accumulated_reward))
+    
+    
+    
+#Test    
 
-
-# In[817]:
-
-
-P={'Sleep':{'Wake up':0.7,'Eat':0.3},
-   'Wake up':{'Eat':1},
-   'Eat':{'Sleep':0.5,'Eat':0.5}}
-S=list(P)
-
-
-# In[818]:
-
-
-M=MP(P,None,True)
-
-
-# In[821]:
-
-
-M.Simulate(10,2,False)
-
-
-# In[ ]:
-
-
-
+P:Transitions={'C1':{'C2':0.5, 'FB':0.5},
+   'C2':{'C3':0.8,'Sleep':0.2},
+   'C3':{'Pass':0.6,'Pub':0.4},
+   'Pass':{'Sleep':1},
+   'Pub':{'C1':0.2,'C2':0.4,'C3':0.4},
+   'FB':{'C1':0.1,'FB':0.9},
+   'Sleep':{'Sleep':1}
+}
 
