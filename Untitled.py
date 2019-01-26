12f6d541cffcd269276344770aef2f8c0ee6c859
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-26 10:54:36 -0800

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
-
 import numpy as np
+from typing import TypeVar, Dict, List, Tuple
 
-Pm=np.array((4,41,55,36,21,43,16,28,56)).reshape(3,3)/100
-Pm=np.array((26,37,37,9,21,70,23,28,49)).reshape(3,3)/100
-Pm=np.array((38,37,25,49,6,45,16,22,62)).reshape(3,3)/100
-Pm=np.array((15,13,72,62,2,36,10,63,27)).reshape(3,3)/100
+Transitions_Rewards_Action_B=Dict[State,Dict[Action,Tuple[State,PR]]]
 
 
-# In[5]:
+class MDP_B(MRP_B):
+    def __init__(self,ProbDist:Transitions_Rewards_Action_B=None,gamma:float=1):
+        self.States=list(ProbDist)
+        self.gamma=gamma
+        self.all_info=ProbDist
+        actions=[]
+        for i in P:
+            for j in P[i]:
+                actions.append(j)
+        self.Actions=sorted(list(set(actions)))
+        self.Actiondict={i:{k for k in self.all_info[i]} for i in self.all_info}
+        
+    def Get_MRP(self,Pol:Policy):
+        ProbDist=np.zeros((len(self.States),len(self.States)))
+        rew=np.zeros((len(self.States),len(self.States)))
+        for i in self.all_info:
+            for j in self.all_info[i]:
+                for k in self.all_info[i][j]:
+                    ProbDist[list(self.all_info).index(i)][list(self.all_info).index(k)] += (self.all_info[i][j][k][0]*Pol[i][j])
+                    rew[list(self.all_info).index(i)][list(self.all_info).index(k)] += (self.all_info[i][j][k][1]*Pol[i][j])
+        return(MRP_B(ProbDist,self.gamma,rew,self.States))
 
 
-Pm
-
-
-# In[ ]:
-
-
-
+#Test -- (Same Policy as for MDP_A)
+P:Transitions_Rewards_Action_B={
+    'Food':{
+        'a':{'Food':(0.4,1), 'Game':(0.6,2)},
+        'b':{'Food':(1/3,3),'Sleep':(1/3,4),'Game':(1/3,5)},
+        'c':{'Sleep':(1,6)}
+    },
+    'Sleep':{
+        'a':{'Sleep':(0.2,7), 'Game':(0.8,9)},
+        'c':{'Sleep':(0.7,9), 'Game':(0.3,10)}
+    },
+    'Game':{
+        'a':{'Food':(0.9,-1), 'Sleep':(0.1,-2)},
+        'b':{'Food':(1/3,-3),'Sleep':(1/3,-4),'Game':(1/3,-5)}
+    }
+}
 
