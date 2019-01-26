0078dcd8fe8c65fed1ea606f8c50642aaad0b6d6
julu123 <44989042+julu123@users.noreply.github.com>
2019-01-25 20:11:59 -0800

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
-
 import numpy as np
+from typing import TypeVar, Dict, List, Tuple
 
-Pm=np.array((4,41,55,36,21,43,16,28,56)).reshape(3,3)/100
-Pm=np.array((26,37,37,9,21,70,23,28,49)).reshape(3,3)/100
-Pm=np.array((38,37,25,49,6,45,16,22,62)).reshape(3,3)/100
-Pm=np.array((15,13,72,62,2,36,10,63,27)).reshape(3,3)/100
+Action = TypeVar('Action')
+Transitions_Rewards_Action_A=Dict[State,Dict[Action,Dict[Tuple[State,(float or int)],(float or int)]]]
+Policy = Dict[State,Tuple[Action,(float or int)]]
 
-
-# In[5]:
-
-
-Pm
-
-
-# In[ ]:
-
-
-
-
+class MDP_A(MRP_A):
+    def __init__(self,ProbDist:Transitions_Rewards_Action_A=None,gamma:float=1):
+        self.States=list(ProbDist)
+        self.gamma=gamma
+        self.all_info=ProbDist
+        pass
+    
+    def Get_MRP(self,Pol:Policy):
+        ProbDist=np.zeros((len(self.States),len(self.States)))
+        rew=np.zeros((len(self.States),1))
+        for i in self.all_info:
+            for j in self.all_info[i]:
+                rew[list(self.all_info).index(i)] += self.all_info[i][j][1]*Pol[i][j]
+                for k in self.all_info[i][j][0]:
+                    ProbDist[list(self.all_info).index(i)][list(self.all_info).index(k)] += (self.all_info[i][j][0][k]*Pol[i][j])
+        rewards = [float(i) for i in rew]
+        return(MRP_A(ProbDist,self.gamma,rewards,self.States))
+    
+    def Get_Optimal_Policy(self):
+        pass
+      
+#Test
+P:Transitions_Rewards_Action_A={
+    'Food':{
+        'a':({'Food':0.4, 'Game':0.6},-2),
+        'b':({'Food':1/3,'Sleep':1/3,'Game':1/3},-5),
+        'c':({'Sleep':1},0)
+    },
+    'Sleep':{
+        'a':({'Sleep':0.2, 'Game':0.8},2),
+        'c':({'Sleep':0.7, 'Game':0.3},0)
+    },
+    'Game':{
+        'a':({'Food':0.9, 'Sleep':0.1},-1),
+        'b':({'Food':1/3,'Sleep':1/3,'Game':1/3},2)
+    }
+}
+Pol:Policies={
+    'Food':{'a':0.5,'b':0.25,'c':0.25},
+    'Sleep':{'a':0.8,'c':0.2},
+    'Game':{'a':0,'b':1}
+}
