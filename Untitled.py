ffde926c687b8a54a839296f123a536859d53b2d
julu123 <44989042+julu123@users.noreply.github.com>
2019-02-10 09:49:47 -0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+
+Pm=np.array((4,41,55,36,21,43,16,28,56)).reshape(3,3)/100
+Pm=np.array((26,37,37,9,21,70,23,28,49)).reshape(3,3)/100
+Pm=np.array((38,37,25,49,6,45,16,22,62)).reshape(3,3)/100
+Pm=np.array((15,13,72,62,2,36,10,63,27)).reshape(3,3)/100
+
+
+# In[5]:
+
+
+Pm
+
+
+# In[ ]:
+
+
+
+
