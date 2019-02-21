87befb6aeb196128bc86f0cdf473cb5d1225baec
julu123 <julu1@stanford.edu>
2019-02-20 19:48:50 -0800

    Update

--- Src/MRP_A.py
+++ Src/MRP_A.py
@@ -7,9 +7,7 @@
 import numpy as np
 from typing import TypeVar, Dict, List, Tuple
 from MP import MP
-
-Transitions_rewards = Dict[Transitions,(float or int)] # For A where each state has a R(s) 
-R_A = List[float]
+from Variables import State, States, Transitions, Transitions_rewards, R_A
 
 
 class MRP_A(MP):
--- Src/Variables.py
+++ Src/Variables.py
@@ -1,5 +1,23 @@
 from typing import TypeVar, Dict, List, Tuple
 
+#MP
 State = TypeVar('State')
 States = List[State]
 Transitions = Dict[State,Tuple[State,(int or float)]]
+
+#MRP_A
+Transitions_rewards = Dict[Transitions,(float or int)] # For A where each state has a R(s)
+R_A = List[float]
+
+#MRP_B
+PR=Tuple[(float or int),(float or int)]
+Transitions_rewards_B = Dict[State,Dict[State,PR]]
+R_B = List[List[(float or int)]]
+
+#MDP_A
+Action = TypeVar('Action')
+Transitions_Rewards_Action_A=Dict[State,Dict[Action,Dict[Dict[State,(float or int)],(float or int)]]]
+Policy = Dict[State,Dict[Action,(float or int)]]
+
+#MDP_B
+Transitions_Rewards_Action_B=Dict[State,Dict[Action,Tuple[State,PR]]]
