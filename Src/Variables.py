11ec1949a1ea56889bc922fd300fffa99e210c58
julu123 <julu1@stanford.edu>
2019-02-20 20:02:04 -0800

    Update Variables.py

--- Src/Variables.py
+++ Src/Variables.py
@@ -9,15 +9,15 @@ Transitions = Dict[State,Tuple[State,(int or float)]]
 Transitions_rewards = Dict[Transitions,(float or int)] # For A where each state has a R(s)
 R_A = List[float]
 
-#MRP_B.py
+#MRP_B
 PR=Tuple[(float or int),(float or int)]
 Transitions_rewards_B = Dict[State,Dict[State,PR]]
 R_B = List[List[(float or int)]]
 
-#MDP_A.py
+#MDP_A
 Action = TypeVar('Action')
 Transitions_Rewards_Action_A=Dict[State,Dict[Action,Dict[Dict[State,(float or int)],(float or int)]]]
 Policy = Dict[State,Dict[Action,(float or int)]]
 
-#MDP_B.py
+#MDP_B
 Transitions_Rewards_Action_B=Dict[State,Dict[Action,Tuple[State,PR]]]
